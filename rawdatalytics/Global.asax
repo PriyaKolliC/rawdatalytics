a4df7ef4538b33d1307e0efa245786029e9234e9
PriyaKolliC <lakshmipriya.kolli1@gmail.com>
2020-07-10 00:48:18 -0400

    Add project files.

---
+++ rawdatalytics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="rawdatalytics.MvcApplication" Language="C#" %>
---
+++ rawdatalytics/Scripts/_Layout.js
@@ -0,0 +1,14 @@
+﻿window.onscroll = function () {
+    myFunction()
+};
+
+var navbar = document.getElementById("nav1");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("navbar-fixed-top")
+    } else {
+        navbar.classList.remove("navbar-fixed-top");
+    }
+}
