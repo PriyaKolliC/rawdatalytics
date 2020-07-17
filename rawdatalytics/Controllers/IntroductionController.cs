e3b1e9b5073e7e6bc1ccd90e720ee1165768ea85
PriyaKolliC <lakshmipriya.kolli1@gmail.com>
2020-07-17 00:52:19 -0400

    07_17_2020

---
+++ rawdatalytics/Controllers/IntroductionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rawdatalytics.Controllers
+{
+    public class IntroductionController : Controller
+    {
+        // GET: Introduction
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DataScientists()
+        {
+            return View();
+        }
+        public ActionResult Difference_ML_AI()
+        {
+            return View();
+        }
+        public ActionResult Lifecycle()
+        {
+            return View();
+        }
+    }
+}
