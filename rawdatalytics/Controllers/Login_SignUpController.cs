9a0c92f910794436bdde7c542be2a3b509d3a695
PriyaKolliC <lakshmipriya.kolli1@gmail.com>
2020-07-23 23:30:46 -0400

    login form added

---
+++ rawdatalytics/Controllers/Login_SignUpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rawdatalytics.Controllers
+{
+    public class Login_SignUpController : Controller
+    {
+        // GET: Login_SignUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ rawdatalytics/Scripts/Login_SignUp/Login_SignUp.js
@@ -0,0 +1,11 @@
+﻿function display_register_form() {
+    var lfc = document.getElementById("login_form_content");
+    var rfc = document.getElementById("register_form_content");
+    lfc.style.display = "none";
+    rfc.style.display = "block";
+}
+$(document).ready(function () {
+    var rfc = document.getElementById("register_form_content");
+    rfc.style.display = "none";
+    lfc.style.display = "block";
+});
